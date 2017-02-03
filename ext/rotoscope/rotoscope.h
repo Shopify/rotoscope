61090d42fd0f96327cfd7f0c661b5eece8df9e7d
Jahfer Husain <jahfer.husain@shopify.com>
2017-02-03 12:11:07 -0500

    Add CSV header and cleanup methods

--- ext/rotoscope/rotoscope.h
+++ ext/rotoscope/rotoscope.h
@@ -4,7 +4,9 @@
 #define EVENT_CALL   (RUBY_EVENT_CALL | RUBY_EVENT_C_CALL)
 #define EVENT_RETURN (RUBY_EVENT_RETURN | RUBY_EVENT_C_RETURN)
 
-typedef enum { false, true } bool;
+#define RS_CSV_VALUES(trace) trace.event, trace.method_owner, trace.method_name, trace.filepath, trace.lineno
+#define RS_CSV_HEADER "event,method_owner,method_name,filepath,lineno\n"
+#define RS_CSV_FORMAT "%s,\"%s\",\"%s\",\"%s\",%d\n"
 
 typedef struct {
   const char* event;
