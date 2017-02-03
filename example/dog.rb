406fc139310ce57c8d1f5b1884de3893ba80eb98
Jahfer Husain <jahfer.husain@shopify.com>
2017-02-03 11:36:20 -0500

    Properly read/format singleton classes

---
+++ example/dog.rb
@@ -0,0 +1,21 @@
+require 'rotoscope'
+
+class Dog
+  def bark
+    Noisemaker.speak('woof!')
+  end
+end
+
+class Noisemaker
+  def self.speak(str)
+    puts(str)
+  end
+end
+
+gzip_file = File.expand_path('tmp/dog_trace.log.gz')
+
+puts "Writing to #{gzip_file}..."
+Rotoscope.trace(gzip_file) do
+  dog1 = Dog.new
+  dog1.bark
+end
