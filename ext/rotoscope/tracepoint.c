411bd462664e55c6ecf2b7e79be5f1ab767cd3ec
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2017-11-06 13:11:59 -0500

    Revert "Merge pull request #48 from Shopify/v8-segfault-debug"
    This reverts commit e747e0a7c2cd2e7803ca0358a4555cc2d6e13e33, reversing
    changes made to d5d750d94e528b156387dee6e4888b922da542b1.

--- ext/rotoscope/tracepoint.c
+++ ext/rotoscope/tracepoint.c
@@ -1,26 +1,8 @@
 #include "tracepoint.h"
 #include "ruby.h"
 
-rs_raw_tracepoint_t rs_raw_from_tracepoint(VALUE tracepoint) {
-  rb_trace_arg_t *trace_arg = rb_tracearg_from_tracepoint(tracepoint);
-  return (rs_raw_tracepoint_t){
-      .self = rb_tracearg_self(trace_arg),
-      .method_id = rb_tracearg_method_id(trace_arg),
-  };
-}
-
-void rs_raw_tracepoint_mark(rs_raw_tracepoint_t *tracepoint) {
-  rb_gc_mark(tracepoint->method_id);
-  rb_gc_mark(tracepoint->self);
-}
-
 void rs_tracepoint_mark(rs_tracepoint_t *tracepoint) {
   rb_gc_mark(tracepoint->entity);
   rb_gc_mark(tracepoint->filepath);
   rb_gc_mark(tracepoint->method_name);
-  rs_raw_tracepoint_mark(&tracepoint->raw);
-}
-
-bool rs_raw_tracepoint_cmp(rs_raw_tracepoint_t *tp1, rs_raw_tracepoint_t *tp2) {
-  return (tp1->method_id == tp2->method_id) && (tp1->self == tp2->self);
 }
