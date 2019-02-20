22081b52a23bfce5dd43715a9b4b84a3f2e42ef5
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2019-02-20 14:20:43 -0500

    Replace empty string filepath default with nil

--- ext/rotoscope/callsite.c
+++ ext/rotoscope/callsite.c
@@ -2,13 +2,10 @@
 #include <ruby.h>
 #include <ruby/debug.h>
 
-VALUE empty_ruby_string;
-
 rs_callsite_t c_callsite(rb_trace_arg_t *trace_arg) {
   VALUE path = rb_tracearg_path(trace_arg);
   return (rs_callsite_t){
-      .filepath = NIL_P(path) ? empty_ruby_string : path,
-      .lineno = FIX2INT(rb_tracearg_lineno(trace_arg)),
+      .filepath = path, .lineno = FIX2INT(rb_tracearg_lineno(trace_arg)),
   };
 }
 
@@ -21,17 +18,11 @@ rs_callsite_t ruby_callsite(rb_trace_arg_t *trace_arg) {
   // frame.
   if (rb_profile_frames(0, 2, frames, lines) < 2) {
     return (rs_callsite_t){
-        .filepath = empty_ruby_string, .lineno = 0,
+        .filepath = Qnil, .lineno = 0,
     };
   }
 
   return (rs_callsite_t){
       .filepath = rb_profile_frame_path(frames[1]), .lineno = lines[1],
   };
-}
-
-void init_callsite() {
-  empty_ruby_string = rb_str_new_literal("");
-  RB_OBJ_FREEZE(empty_ruby_string);
-  rb_global_variable(&empty_ruby_string);
 }
